b202729c1419ba02a81de90142c3d06e52f0dc09
AkarshRinku <86792553+AkarshRinku@users.noreply.github.com>
2021-07-22 13:17:30 +0530

    my name is akarsh

---
+++ LineComparison/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC_1CheckLength.Cartesian();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LineComparison/UC_1CheckLength.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparison
+{
+    class UC_1CheckLength
+    {
+        public static void Cartesian()
+        {
+            double a1, a2, b1, b2;
+            double c1, c2, d1, d2;
+            double length1, length2;
+            Console.WriteLine("Enter the points of line1");
+            Console.WriteLine("Enter points (a1,b1): ");
+            a1 = double.Parse(Console.ReadLine());
+            b1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter points (a2,b2): ");
+            a2 = double.Parse(Console.ReadLine());
+            b2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the points of line2");
+            Console.WriteLine("Enter points (c1,d1): ");
+            c1 = double.Parse(Console.ReadLine());
+            d1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter points (c2,d2): ");
+            c2 = double.Parse(Console.ReadLine());
+            d2 = double.Parse(Console.ReadLine());
+            length1 = Math.Sqrt(((a2 - a1) * (a2 - a1)) + ((b2 - b1) * (b2 - b1)));
+            length2 = Math.Sqrt(((c2 - c1) * (c2 - c1)) + ((d2 - d1) * (d2 - d1)));
+            Console.WriteLine("The length between two points are:" + length1 + length2);
+        }
+    }
+
+}
+
